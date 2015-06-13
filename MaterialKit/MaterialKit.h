c674d815ca94893b44be2cf13b3f636d351d6300
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-14 00:47:25 +0900

    Add dynamic framework project

---
+++ MaterialKit/MaterialKit.h
@@ -0,0 +1,19 @@
+//
+//  MaterialKit.h
+//  MaterialKit
+//
+//  Created by 林達也 on 2015/06/14.
+//  Copyright (c) 2015年 MaterialKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialKit.
+FOUNDATION_EXPORT double MaterialKitVersionNumber;
+
+//! Project version string for MaterialKit.
+FOUNDATION_EXPORT const unsigned char MaterialKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialKit/PublicHeader.h>
+
+
